e3cb4a5e55ec47712d78c87691cae3957d1f9000
Luviana <lutfianadewi5@gmail.com>
2016-09-20 04:01:12 +0700

    Layout sudah dibuat namun masih belum sempurna

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2014/myproject/MainActivity.java
@@ -0,0 +1,16 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl2014.myproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+    }
+}
